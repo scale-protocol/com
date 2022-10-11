6a24e00e91ed67be53f0b26e62392b1abdb6f16b
xjj <xujingjing@koolio.exchange>
2022-10-11 14:45:44 +0800

    update google-analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,34 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Head from 'next/head'
 
-class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
-    }
+function IndexPage() {
+  return (
+    <Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-35R4FYLXJV"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
 
-    render() {
-        return (
-            <Html>
-                <Head>
-                  <script async src="https://www.googletagmanager.com/gtag/js?id=G-35R4FYLXJV"></script>
-                    <script dangerouslySetInnerHTML={{
-                        __html:`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
-                        gtag('config', 'G-35R4FYLXJV');
-                        `,
-                    }} />
-
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+        gtag('config', 'G-35R4FYLXJV');
+      </script>
+    </Head>
+  )
 }
 
-export default MyDocument
+export default IndexPage
